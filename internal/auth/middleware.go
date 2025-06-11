b02aa0e612279e1b65ddec4cee390bf513240a2f
ryeom <sa2who2@naver.com>
2025-06-11 23:49:46 +0900

    commit

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,1 +1,33 @@
 package auth
+
+import (
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func JWTMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		tokenStr := c.Request().Header.Get("Authorization")
+		if !strings.HasPrefix(tokenStr, "Bearer ") {
+			return c.JSON(http.StatusUnauthorized, "no token provided")
+		}
+
+		tokenStr = strings.TrimPrefix(tokenStr, "Bearer ")
+		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+		if err != nil {
+			return c.JSON(http.StatusUnauthorized, "invalid token")
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("userID", claims["userID"])
+			return next(c)
+		}
+
+		return c.JSON(http.StatusUnauthorized, "invalid token")
+	}
+}
