618c023e05cf90c9241c9add8fdc63917261aa3d
ryeom <sa2who2@naver.com>
2025-07-06 23:18:15 +0900

    commit

--- server/server.go
+++ server/server.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Ryeom/board-game/infra/mongo"
 	redisutil "github.com/Ryeom/board-game/infra/redis"
 	"github.com/Ryeom/board-game/internal/auth"
+	resp "github.com/Ryeom/board-game/internal/response"
 	"github.com/Ryeom/board-game/internal/util"
 	l "github.com/Ryeom/board-game/log"
 	appHttp "github.com/Ryeom/board-game/server/http"
@@ -14,6 +15,8 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoSwagger "github.com/swaggo/echo-swagger"
+	"net/http"
+	"time"
 )
 
 func Initialize(e *echo.Echo) {
@@ -36,9 +39,59 @@ func Initialize(e *echo.Echo) {
 	ws.GlobalBroadcaster = ws.NewRedisBroadcaster(ctx)
 
 }
-
 func httpErrorHandler(e *echo.Echo) func(err error, c echo.Context) {
 	return func(err error, c echo.Context) {
+		var code string
+		var statusCode int = http.StatusInternalServerError
+		var originalErr error = err
 
+		httpErr, ok := err.(*echo.HTTPError)
+		if ok {
+			statusCode = httpErr.Code
+			//message := fmt.Sprint(httpErr.Message)
+
+			switch statusCode {
+			case http.StatusBadRequest:
+				code = resp.ErrorCodeDefaultBadRequest
+			case http.StatusUnauthorized:
+				code = resp.ErrorCodeDefaultUnauthorized
+			case http.StatusForbidden:
+				code = resp.ErrorCodeDefaultForbidden
+			case http.StatusNotFound:
+				code = resp.ErrorCodeDefaultNotFound
+			default:
+				code = resp.ErrorCodeDefaultInternalServerError
+			}
+			originalErr = httpErr.Internal
+		} else {
+			// 2. 그 외 일반적인 Go error 또는 panic으로부터 복구된 에러
+			code = resp.ErrorCodeDefaultInternalServerError
+			statusCode = http.StatusInternalServerError
+		}
+
+		response := resp.Fail(code, "ko")
+		response.Timestamp = time.Now()
+
+		if response.Error != nil {
+			response.Error.HttpStatusCode = statusCode
+		} else {
+			response.Status = "error"
+			response.Message = "알 수 없는 오류가 발생했습니다."
+			response.Code = code
+		}
+
+		if originalErr != nil {
+			l.Logger.Errorf("HTTP Error: Path=%s, Status=%d, Code=%s, Message=%s, OriginalError=%v",
+				c.Path(), statusCode, code, response.Message, originalErr)
+		} else {
+			l.Logger.Errorf("HTTP Error: Path=%s, Status=%d, Code=%s, Message=%s",
+				c.Path(), statusCode, code, response.Message)
+		}
+
+		if !c.Response().Committed {
+			if err := c.JSON(statusCode, response); err != nil {
+				l.Logger.Error(err)
+			}
+		}
 	}
 }
