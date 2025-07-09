fc36023a1e7c4af5e36bb666b3576deaa9e5571c
ryeom <sa2who2@naver.com>
2025-07-09 22:57:05 +0900

    commit

--- test/ws_helper.go
+++ test/ws_helper.go
@@ -1,4 +1,4 @@
-// test/ws_helper.go (새 파일)
+// test/ws_helper.go
 package test
 
 import (
@@ -11,10 +11,15 @@ import (
 )
 
 type WSEvent struct {
-	Type   string      `json:"type"`
-	Data   interface{} `json:"data,omitempty"`
-	RoomID string      `json:"roomId,omitempty"`
-	Name   string      `json:"name,omitempty"`
+	Type      string      `json:"type"`
+	Data      interface{} `json:"data,omitempty"`
+	RoomID    string      `json:"roomId,omitempty"`
+	Name      string      `json:"name,omitempty"`
+	Message   string      `json:"message,omitempty"`
+	Success   bool        `json:"success,omitempty"`
+	Code      interface{} `json:"code,omitempty"`
+	ErrorCode string      `json:"errorCode,omitempty"`
+	Action    string      `json:"action,omitempty"`
 }
 
 func SendEvent(t *testing.T, conn *websocket.Conn, event WSEvent) {
@@ -31,7 +36,7 @@ func ReadEvent(t *testing.T, conn *websocket.Conn, timeout time.Duration) WSEven
 
 	var event WSEvent
 	err = json.Unmarshal(msg, &event)
-	assert.NoError(t, err, "Failed to unmarshal message")
+	assert.NoError(t, err, "Failed to unmarshal message: %s", string(msg)) // 실패 시 메시지 출력
 	return event
 }
 
@@ -48,7 +53,7 @@ func IdentifyUser(t *testing.T, conn *websocket.Conn, userID, userName string) {
 
 func ConnectAndIdentify(t *testing.T, wsURL, userID, userName string) *websocket.Conn {
 	dialer := websocket.Dialer{}
-	conn, _, err := dialer.Dial(wsURL+"?id="+userID+"&name="+userName, nil)
+	conn, _, err := dialer.Dial(wsURL+"?id="+userID+"&name="+userName, nil) // id, name 쿼리 파라미터는 websocket.go에서 사용하지 않으므로 제거하거나 유의
 	assert.NoError(t, err)
 
 	IdentifyUser(t, conn, userID, userName)
